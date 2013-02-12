b63bc0d5d62012157ad61d674244374da6ad234c
joseEmmanuel <emmanuelfrias-91@hotmail.com>
2013-02-12 13:33:26 -0600

    Modificado

--- Practica1.java
+++ Practica1.java
@@ -1,23 +1,23 @@
 import java.util.Random;
 import java.util.Scanner;
 
-
+//Trabajo//
 public class Practica1 {
 
-	
+
 	public static void main(String[] args) {
 		// Crear un Arreglo de 30 posiciones con numeros entre el 15 y el 30 aleatoriamente
 		int[] a = new int[30];
-		
+
 		Random r = new Random();
-		
+
 		for(int i=0; i<30; i++){
 			a[i] = r.nextInt (15) + 15;
 			System.out.print(a[i]+",");
 		}
 
 	}
-	
+
 //Capturar el valor de un articulo, calcular el IVA y mostrar el precio total
 System.out.printIn("\nPROGRAMA PARA CALCULAR IVA");
 
